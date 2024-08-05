0384151b660a85aefc921ab63749b8feab32c8de
gluke2023 <grischenko.is@bratsk-city.ru>
2024-08-05 10:19:50 +0800

    Удалил кнопки из вьюхи. Сделал переход по клику на само сообщение.

---
+++ TestTC/wwwroot/assets/js/tableRowClick.js
@@ -0,0 +1,14 @@
+﻿
+
+document.addEventListener('DOMContentLoaded', function () {
+    const rows = document.querySelectorAll('.list-selected');
+
+    rows.forEach(row => {
+        row.addEventListener('click', function () {
+            const id = this.getAttribute('data-id');
+            if (id) {
+                window.location.href = `/ToDoItem/Show/${id}`;
+            }
+        });
+    });
+});
